3a773019cbf3db0d44c4b68ca44672a22c3f3950
Gabriel Araujo <gabriel.houth@gmail.com>
2021-11-03 00:26:46 -0300

    feat: migrations now follows laravel good practices

--- database/migrations/2021_11_03_021902_create_auths_table.php
+++ database/migrations/2021_11_03_021902_create_auths_table.php
@@ -9,22 +9,11 @@ class CreateAuthsTable extends Migration
 {
     public function up()
     {
-<<<<<<< HEAD:database/migrations/2021_11_03_021902_create_auths_table.php
         Schema::create('auths', function (Blueprint $table) {
             $table->increments('id');
             $table->string('userLogin', 30);
             $table->string('userPassword', 30);
         });
-=======
-        DB::statement("
-        CREATE TABLE auths (
-            id INTEGER NOT NULL AUTO_INCREMENT,
-            userLogin VARCHAR(30),
-            userPassword VARCHAR(30),
-            CONSTRAINT pk_auths PRIMARY KEY (id)
-          );
-        ");
->>>>>>> 6f9848e1452627e791c1b576acd5dac0de0072fe:database/migrations/2021_11_02_140248_create_auths_table.php
     }
 
     public function down()
